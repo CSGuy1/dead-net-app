1c77c517850688d26a5463fa21e50a339725e319
Ben <bds0027@mix.wvu.edu>
2022-01-24 13:50:28 -0500

    Started header component and added dead.net header logo to assets folder

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'dead-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
+})
+export class FooterComponent{
+
+}
