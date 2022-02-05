6570869fd8863c61b73106ef57aad0c9cdd813ad
Ben <bds0027@mix.wvu.edu>
2022-02-05 12:40:25 -0500

    Added data binding to the horizontal card component

--- src/app/Horizontal Card/horizontal.card.component.ts
+++ src/app/Horizontal Card/horizontal.card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "dead-card-horizontal",
@@ -7,5 +7,11 @@ import { Component } from "@angular/core";
 })
 
 export class HorizontalCardComponent{
+    @Input() cardDate: string;
+    @Input() cardDescription: string;
 
+    constructor() {
+        this.cardDate = "Month Day, Year";
+        this.cardDescription = "MISSING DESCRIPTION";
+    }
 }
