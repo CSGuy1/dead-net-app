d5a0fdc300cc75032d6c1cb6398c6999975fb7e6
Ben <bds0027@mix.wvu.edu>
2022-04-09 16:58:15 -0400

    Added user authentication to web app

--- src/app/auth/authentication/authentication.component.ts
+++ src/app/auth/authentication/authentication.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
+import { AuthResponse } from './authResponse';
 
 @Component({
   selector: 'dn-authentication',
@@ -8,24 +10,33 @@ import { AuthenticationService } from './authentication.service';
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
+  public buttonClicked!: string;
+  private authObservable!: Observable<AuthResponse>;
 
-  constructor(private auth:AuthenticationService) { }
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(data:NgForm){
-    console.log("Button clicked");
+  onSubmit(data: NgForm) {
+    console.log("Button clicked = " + this.buttonClicked);
     console.log(data.value);
-    data.reset();
 
-    this.auth.signup(data.value.email, data.value.password).subscribe(data => {
+    if (this.buttonClicked == "Signup") {
+      this.authObservable = this.auth.signup(data.value.email, data.value.password);
+    } else if (this.buttonClicked == "Login") {
+      this.authObservable = this.auth.login(data.value.email, data.value.password);
+    }
+
+    this.authObservable.subscribe(data => {
       console.log(data);
 
     },
-    error => {
-      console.log(error);
-    }
+      error => {
+        console.log(error);
+      }
     )
+
+    data.reset();
   }
 }
--- src/app/auth/authentication/authentication.service.ts
+++ src/app/auth/authentication/authentication.service.ts
@@ -1,23 +1,35 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { AuthResponse } from "./authResponse"
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthenticationService{
+export class AuthenticationService {
     baseUrl: string = "https://identitytoolkit.googleapis.com/v1/accounts";
     signUpEndPoint: string = "signUp";
+    signInEndPoint: string = "signInWithPassword"
 
-    public constructor(private http:HttpClient) {}
+    public constructor(private http: HttpClient) { }
 
-    public signup(email:string, password:string) {
+    public signup(email: string, password: string) {
         const requestBody = {
             "email": email,
             "password": password,
             "returnSecureToken": true
         };
 
-        return this.http.post(this.baseUrl + ":" + this.signUpEndPoint + "?" + "key=" + environment.firebase.apiKey, requestBody);
+        return this.http.post<AuthResponse>(this.baseUrl + ":" + this.signUpEndPoint + "?" + "key=" + environment.firebase.apiKey, requestBody);
+    }
+
+    public login(email: string, password: string) {
+        const requestBody = {
+            "email": email,
+            "password": password,
+            "returnSecureToken": true
+        };
+
+        return this.http.post<AuthResponse>(this.baseUrl + ":" + this.signInEndPoint + "?" + "key=" + environment.firebase.apiKey, requestBody);
     }
 }
